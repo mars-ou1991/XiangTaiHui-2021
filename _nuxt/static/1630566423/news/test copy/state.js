9b64cc202283b5e999d728c72a6730f8d177869a
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-09-02 07:36:19 +0000

    deploy: 0c66b7921053cd773cd5fa90e3c9676e079358c4

--- _nuxt/static/1630566423/news/test copy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FXiangTaiHui-2021\u002F_nuxt\u002Fstatic\u002F1630566423",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnews\u002Ftest%20copy",config:{_app:{basePath:"\u002FXiangTaiHui-2021\u002F",assetsPath:"\u002FXiangTaiHui-2021\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7e9dceb7"}}}}(null));
