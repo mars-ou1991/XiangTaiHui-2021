45ff89001cd62c7785ca54c2ddcb793a739544a8
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-09-02 06:51:26 +0000

    deploy: 9f3a2fb84852c7c26518d89811106ead2d4770ae

---
+++ _nuxt/ff79988.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{423:function(e,n,t){"use strict";t.r(n);var c={},l=t(51),component=Object(l.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("mobile")])}),[],!1,null,"491644a5",null);n.default=component.exports}}]);
---
+++ _nuxt/static/1630565484/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhome","\u002Fhome-mobile","\u002Fnews\u002Ftest copy","\u002Fnews\u002Ftest"]})
