d6687b732349c577e3bc4aa16b31def1229d7ece
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-09-02 07:07:05 +0000

    deploy: 27a3724f29c0f6eefab69bf08de44ef489dd0407

---
+++ _nuxt/static/1630566423/home-mobile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FXiangTaiHui-2021\u002F_nuxt\u002Fstatic\u002F1630566423",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome-mobile",config:{_app:{basePath:"\u002FXiangTaiHui-2021\u002F",assetsPath:"\u002FXiangTaiHui-2021\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7e9dceb7"}}}}(null));
