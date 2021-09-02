27a3724f29c0f6eefab69bf08de44ef489dd0407
ouyangshun <ouyangshun@qianxin.com>
2021-09-02 15:05:38 +0800

    modify config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/<repository-name>/'
+    base: '/XiangTaiHui-2021/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
